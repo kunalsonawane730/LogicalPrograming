ba1a870f7b20d90c5645ffcb657de172b560d477
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 00:49:06 +0530

    [kunal]commiting changes for FibonacciSeries program

---
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -0,0 +1,7 @@
+﻿using LogicalProgram;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+FibonacciSeries series = new FibonacciSeries();
+series.FindFibonacciSeries();
+
